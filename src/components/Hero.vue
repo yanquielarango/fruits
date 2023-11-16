ac98fca5a849d546cb3128a3cd8dcf4a9f4d9aa5
yanquielarango <yanquiel.arango@gmail.com>
2023-11-16 20:16:24 +0100

    adding Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,38 @@
+<script setup>
+  
+</script>
+
+<template>
+  <main class="main">
+
+    <section class="home section" id="home">
+      <div class="home__container container grid">
+        <div class="home__data">
+          <h1 class="home__title"> 
+            Enjoy The <br>
+            Best Fresh <br>
+            Fruits
+          </h1>
+
+          <p class="home__description">
+            Enjoy the best organic fruits harvested with 
+            the best possible quality, obtaining unique
+            and delicious flavours.
+          </p>
+
+          <a href="#" class="button">
+            Order Fruit Now
+          </a>
+
+          <img src="../assets/fruit-line-2.png" alt="image" class="home__fruit"  />
+          
+        </div>
+
+        <img  src="../assets/home-fruits.png"  alt="image" class="home__img"/>        
+      </div>
+
+      <img src="../assets/branch-2.png" alt="image" class="home__branch" />
+    </section>
+    
+  </main>
+</template>
